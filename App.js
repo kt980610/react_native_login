917008bc79a8db0777c5592cf4e20db0d8d30891
kt980610 <krm.tn1998@gmail.com>
2022-09-05 15:08:41 +0300

    commit deneme

---
+++ App.js
@@ -0,0 +1,140 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Dimensions,
+  StyleSheet,
+  Image,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return(splashScreen());
+};
+const {height} = Dimensions.get("screen");
+const height_logo = height * 0.28;
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  container: {
+    flex: 1, 
+    backgroundColor: '#009387'
+  },
+  header: {
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  footer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      paddingVertical: 50,
+      paddingHorizontal: 30
+  },
+  logo: {
+      width: height_logo,
+      height: height_logo
+  },
+  title: {
+      color: '#05375a',
+      fontSize: 30,
+      fontWeight: 'bold'
+  },
+  text: {
+      color: 'grey',
+      marginTop:5
+  },
+  button: {
+      alignItems: 'flex-end',
+      marginTop: 30
+  },
+  signIn: {
+      width: 150,
+      height: 40,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 50,
+      flexDirection: 'row'
+  },
+  textSign: {
+      color: 'white',
+      fontWeight: 'bold'
+  },
+});
+const Cat = () => {
+  return <Text>Hello, I am your cat!</Text>;
+};
+const splashScreen = () => {
+  return(<><><View style={styles.sectionContainer}></View><View style={styles.Header}></View><Image source={{ uri: 'https://github.com/itzpradip/react-navigation-v5-mix/blob/master/assets/logo.png?raw=true' }} style={styles.logo} resizeMode ="stretch" /></><Text>Hello, I am your cat!</Text></>
+  );
+};
+
+export default App;
